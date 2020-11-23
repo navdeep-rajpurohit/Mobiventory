117c33b8aa693b9f542c1e7629ca7a3d43ac31d7
navdeep-rajpurohit <navdeep.raj.bkn@gmail.com>
2020-11-23 21:43:53 +0530

    Hey

--- app/src/main/java/com/example/mobiventory/MainActivity.java
+++ app/src/main/java/com/example/mobiventory/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mobiventory;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void loginact(View view) {
+        Intent intent = new Intent(this, login.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void signupact(View view) {
+        Intent intent = new Intent(this, signup.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
