df0b5702649e256df1dcbda5cf9327a52b020947
Vishal <vsvishalsinha123@gmail.com>
2020-12-04 23:13:04 +0530

    ab

--- app/src/main/java/com/example/mobiventory/dashboard.java
+++ app/src/main/java/com/example/mobiventory/dashboard.java
@@ -1,6 +1,7 @@
 package com.example.mobiventory;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,9 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Toolbar toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 }
--- app/src/main/java/com/example/mobiventory/login.java
+++ app/src/main/java/com/example/mobiventory/login.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class login extends AppCompatActivity {
     EditText username;
@@ -14,13 +15,15 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        EditText username = (EditText)findViewById(R.id.usernameTB);
-        EditText password = (EditText)findViewById(R.id.passwordTB);
+ //       EditText username = (EditText)findViewById(R.id.usernameTB);
+  //      EditText password = (EditText)findViewById(R.id.passwordTB);
     }
 
     public void dashboardact(View view) {
         Intent intent = new Intent(this, dashboard.class);
          startActivity(intent);
+        Toast msg = Toast.makeText(getBaseContext(), "Loggged in.", Toast.LENGTH_LONG);
+        msg.show();
 //        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
 //            Intent intent = new Intent(this, dashboard.class);
 //            startActivity(intent);
