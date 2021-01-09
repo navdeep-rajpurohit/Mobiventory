b5d487cb713a6431a83181aafd259e5ba3a173b4
Vishal <vsvishalsinha123@gmail.com>
2021-01-09 14:26:22 +0530

    Before_exam_hustle

--- app/src/main/java/com/example/mobiventory/add_catagory.java
+++ app/src/main/java/com/example/mobiventory/add_catagory.java
@@ -3,12 +3,41 @@ package com.example.mobiventory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class add_catagory extends AppCompatActivity {
+
+    EditText catagory;
+    Button add;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_catagory);
+
+        catagory=findViewById(R.id.editText1);
+
+        add=findViewById(R.id.button1);
+        DB = new DBHelper(this);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String catagoryTXT =catagory.getText().toString();
+
+                Boolean checkaddcatagory = DB.addcatagory(catagoryTXT);
+                if(checkaddcatagory==true)
+                    Toast.makeText(add_catagory.this, "New catagory inserted", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(add_catagory.this, "Not inserted", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/mobiventory/add_product.java
+++ app/src/main/java/com/example/mobiventory/add_product.java
@@ -3,12 +3,50 @@ package com.example.mobiventory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class add_product extends AppCompatActivity {
+    EditText pname, pid, quantity, cost_price, sell_price;
+    Button Add;
+    DB1Helper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
+
+        pname =findViewById(R.id.pname);
+        pid =findViewById(R.id.pid);
+        quantity =findViewById(R.id.quantity);
+        cost_price =findViewById(R.id.cost_price);
+        sell_price =findViewById(R.id.sell_price);
+
+        Add=findViewById(R.id.Add);
+        DB = new DB1Helper(this);
+
+        Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pnameTXT =pname.getText().toString();
+                String pidTXT =pid.getText().toString();
+                String quantityTXT =quantity.getText().toString();
+                String cost_priceTXT =cost_price.getText().toString();
+                String sell_priceTXT =sell_price.getText().toString();
+
+                Boolean checkaddproduct = DB.addproduct(pnameTXT, pidTXT,quantityTXT,cost_priceTXT,sell_priceTXT);
+                if (checkaddproduct==true)
+                    Toast.makeText(add_product.this, "New Product Added", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(add_product.this, "Not Added", Toast.LENGTH_SHORT).show();
+
+
+
+
+            }
+        });
+
     }
 }
